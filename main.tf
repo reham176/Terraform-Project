a4560870d2ddf392b6482cfe692403ab7b04b1f3
reham176 <59253530+reham176@users.noreply.github.com>
2024-09-20 17:30:31 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-   region = "us-east-1"
+    region = "us-east-1"
 }
 variable "subnet_cidr_block"  {}
 variable "avil_zone" {} #bas keda whigib el value bta3ha mn env.var eli ana m3rfaha fil terminal
